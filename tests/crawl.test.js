2497c6bdec757c054e536109bf4a9da3cb6a12fc
patrickneise <patrick.neise@gmail.com>
2024-06-15 10:26:42 -0400

    organize src

--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { getURLsFromHTML, normalizeURL } from "./crawl.js";
+import { getURLsFromHTML, normalizeURL } from "../src/crawl";
 
 
 test('normalizeURL https', () => {
